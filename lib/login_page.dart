e81ae5b0e04db134bccfe958ffa7d0f9c6285f27
Guilherme Santos <g_asantos@hotmail.com>
2021-12-09 21:22:12 -0300

    Mostly done visuals

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,12 +18,13 @@ class HomePage extends StatelessWidget {
                   width: double.infinity,
                   alignment: Alignment.topLeft,
                   child: IconButton(
+                    color: Colors.white,
                     icon: const Icon(Icons.arrow_back_ios),
                     onPressed: () {},
                   ),
                 ),
                 const SizedBox(
-                  height: 150,
+                  height: 190,
                 ),
                 SvgPicture.network(
                   'https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-White-Dark-Background-Logo.wine.svg',
@@ -30,73 +32,103 @@ class HomePage extends StatelessWidget {
                       Container(child: const CircularProgressIndicator()),
                 ),
                 const SizedBox(
-                  height: 140,
+                  height: 35,
                 ),
                 RichText(
+                    textAlign: TextAlign.center,
                     text: const TextSpan(
                         style: TextStyle(color: Colors.white),
+                        text:
+                            'By tapping Create Account or Sign In, you agree to our ',
                         children: [
-                      TextSpan(
-                          text:
-                              'By tapping Create Account or Sign In, you agree to our '),
-                      TextSpan(
-                          style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              fontWeight: FontWeight.bold),
-                          text: 'Terms'),
-                      TextSpan(
-                          text: '. Learn how we process your data in our '),
-                      TextSpan(
-                          style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              fontWeight: FontWeight.bold),
-                          text: 'Privacy Policy'),
-                      TextSpan(text: ' and '),
-                      TextSpan(
-                          style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              fontWeight: FontWeight.bold),
-                          text: 'Cookies Policy'),
-                      TextSpan(text: ' .'),
-                    ])),
-                Container(
-                  width: double.infinity,
-                  height: 48,
-                  child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.add),
-                    label: const Text('Sign In with Apple'),
-                    style: ElevatedButton.styleFrom(
-                      primary: const Color(0xffFE3C72),
-                      onPrimary: Colors.white,
-                    ),
-                  ),
+                          TextSpan(
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  decorationThickness: 2,
+                                  fontWeight: FontWeight.bold),
+                              text: 'Terms'),
+                          TextSpan(
+                              text: '. Learn how we process your data in our '),
+                          TextSpan(
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  decorationThickness: 2,
+                                  fontWeight: FontWeight.bold),
+                              text: 'Privacy Policy'),
+                          TextSpan(text: ' and '),
+                          TextSpan(
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  decorationThickness: 2,
+                                  fontWeight: FontWeight.bold),
+                              text: 'Cookies Policy'),
+                          TextSpan(text: '.'),
+                        ])),
+                const SizedBox(
+                  height: 15,
                 ),
                 Container(
                   width: double.infinity,
                   height: 48,
                   child: ElevatedButton.icon(
                     onPressed: () {},
-                    icon: const Icon(Icons.add),
-                    label: const Text('Sign In with Facebook'),
+                    icon: const Icon(MdiIcons.apple),
+                    label: const Text('SIGN IN WITH APPLE'),
                     style: ElevatedButton.styleFrom(
+                      shape: const StadiumBorder(),
+                      side: const BorderSide(
+                        width: 2.0,
+                        color: Colors.white,
+                      ),
                       primary: const Color(0xffFE3C72),
                       onPrimary: Colors.white,
                     ),
                   ),
+                ),
+                const SizedBox(
+                  height: 15,
                 ),
                 Container(
                   width: double.infinity,
                   height: 48,
                   child: ElevatedButton.icon(
                     onPressed: () {},
-                    icon: const Icon(Icons.add),
-                    label: const Text('Sign In with Phone Number'),
+                    icon: const Icon(Icons.facebook),
+                    label: const Text('SIGN IN WITH FACEBOOK'),
                     style: ElevatedButton.styleFrom(
+                      shape: const StadiumBorder(),
+                      side: const BorderSide(
+                        width: 2.0,
+                        color: Colors.white,
+                      ),
                       primary: const Color(0xffFE3C72),
                       onPrimary: Colors.white,
                     ),
                   ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(MdiIcons.chat),
+                    label: const Text('SIGN IN WITH PHONE NUMBER'),
+                    style: ElevatedButton.styleFrom(
+                      shape: const StadiumBorder(),
+                      side: const BorderSide(
+                        width: 2.0,
+                        color: Colors.white,
+                      ),
+                      primary: const Color(0xffFE3C72),
+                      onPrimary: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
                 ),
                 RichText(
                     text: const TextSpan(
@@ -104,6 +136,9 @@ class HomePage extends StatelessWidget {
                         children: [
                       TextSpan(text: 'Trouble Signing In?'),
                     ])),
+                const SizedBox(
+                  height: 15,
+                ),
               ],
             )));
   }
